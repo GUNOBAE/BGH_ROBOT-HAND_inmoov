9f2a46dbc56e94a72ad3c9350f79911efc2dba79
BGH <zmmm777@naver.com>
2026-09-25 18:35:11 +0900

    Create servo_calibration.ino

---
+++ arduino/servo_calibration/servo_calibration.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+Servo myservo;
+int servoPin = 3;
+
+void setup() {
+
+  myservo.attach(servoPin);
+  Serial.begin(9600);
+
+  Serial.println("각도(0~180)를 입력하고 Enter를 누르세요.");
+  myservo.write(90); // 90도에서 시작
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    // 시리얼 포트에서 정수(숫자)를 읽어옴.
+    int angle = Serial.parseInt();
+
+    Serial.print("입력된 각도: ");
+    Serial.println(angle);
+
+    // 읽어온 각도 값으로 서보모터를 움직임.
+    myservo.write(angle);
+
+    //serial 통신간 쓰레기 글자 제거
+    while (Serial.available() > 0) {
+      Serial.read();
+    }
+  }
+}
